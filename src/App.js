da35afb5e759e900b472911211001d9a6eb0a59d
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-09 00:21:52 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import imgFruit from "./components/image/maca.png"
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        name: "David",
+        age: 28,
+        favoriteFood: "Espaguete",
+        favoriteMusic: ["Mutter", "Sonnen", "Amerika"]
+    }
+    render(){
+        return(
+            <div className="all-conteiner">
+                <h1>{this.state.name}</h1>
+                <h2>{this.state.age}</h2>
+                <h3>{this.state.favoriteFood}</h3>
+                <h1>My favorite Songs of the Hammstein</h1>
+                <ul className="conteiner-songs">
+                    <li>{this.state.favoriteMusic[0]}</li>
+                    <li>{this.state.favoriteMusic[1]}</li>
+                    <li>{this.state.favoriteMusic[2]}</li>
+                </ul>
+                <div>
+                    <figure>
+                        <img src={imgFruit}/>
+                        <figcaption>Maça</figcaption>
+                    </figure>
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export default App;
